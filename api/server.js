864af14bb2ae0e0f8f4647c1b7f11727f64b1cdb
Dejviik01 <brawlstars1234567890@seznam.cz>
2026-01-02 20:41:43 +0100

    Add files via upload

---
+++ api/server.js
@@ -0,0 +1,35 @@
+import fetch from "node-fetch";
+import iconv from "iconv-lite";
+
+export default async function handler(req, res) {
+  const server = req.query.server;
+
+  if (!server || !["3", "4", "5", "6", "7"].includes(server)) {
+    res.status(400).json({ error: "Neplatný server" });
+    return;
+  }
+
+  const url = `https://www.panhradu.cz/globe_serialize.aspx?id_server=${server}`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "User-Agent": "Mozilla/5.0"
+      }
+    });
+
+    const buffer = Buffer.from(await response.arrayBuffer());
+    const text = iconv.decode(buffer, "windows-1250");
+
+    // odebrat \r a rozdělit na řádky
+    const lines = text.replace(/\r/g,'').split('\n');
+
+    // vrátit JSON s polem řádků
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    res.status(200).json({ data: lines });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Chyba při načítání dat" });
+  }
+}
